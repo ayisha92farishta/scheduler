5e1bf46fa9990549536ae4ab318a39d7b287c4ba
Ayisha Farishta Ashraf <Ayisha Farishta Ashraf>
2021-11-10 16:37:16 -0500

    Setup Header component under Appointments

--- src/components/Appointments/index.js
+++ src/components/Appointments/index.js
@@ -5,7 +5,6 @@ export default function Appointment(props){
   return (
     <article className="appointment">
      {props.time ? `Appointment at ${props.time}` : "No Appointments"} 
-
     </article>
   )
 }
