92d2f7a7b088895a27627040ddb904fdfe63aab7
Ayisha Farishta Ashraf <Ayisha Farishta Ashraf>
2021-11-10 15:45:12 -0500

    Add ul to Daylist

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,13 +4,15 @@ import DayListItem from "./DayListItem";
 export default function DayList(props) {
   const day = props.days.map(d => {
     return (    
+      <ul> 
       <DayListItem
         key={d.id}
         name={d.name} 
         spots={d.spots} 
         selected={d.name === props.day}
         setDay={props.setDay}
-      />     
+      />
+      </ul>     
     );
   });
   return day;
