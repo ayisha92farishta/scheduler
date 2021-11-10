3a9b15d9eb8739b1543f851326776a3ded49c30f
Ayisha Farishta Ashraf <Ayisha Farishta Ashraf>
2021-11-09 22:45:54 -0500

    Add interviewers using map rfunction

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,12 +1,25 @@
 import React from "react";
 import DayListItem from "./DayListItem";
-import '/InterviewerList.scss'
+import InterviewerListItem from "./InterviewerListItem"
+import './InterviewerList.scss'
 
 export default function InterviewerList (props) {
-  return (
-    <section className="interviewers">
-      <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
-    </section>
-  )
+  const oneInterviewer = props.interviewers.map(i => {
+    return (
+        <InterviewerListItem
+          id={i.id}
+          name={i.name}
+          avatar={i.avatar}
+          selected={i.name === props.day}
+          setInterviewer={props.setInterviewer}
+        />
+    )
+  });
+  return oneInterviewer;
+  // return (
+  //   <section className="interviewers">
+  //     <h4 className="interviewers__header text--light">Interviewer</h4>
+  //     <ul className="interviewers__list"></ul>
+  //   </section>
+  // )
 }
