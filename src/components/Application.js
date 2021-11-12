61beafc0957ee99d6f48b7a925a876f77e33aeb6
Ayisha Farishta Ashraf <Ayisha Farishta Ashraf>
2021-11-11 21:07:52 -0500

    Add hardcoded data to application.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,6 +5,8 @@ import "components/Application.scss";
 
 import DayList from "components/DayList";
 
+
+//hardcoded data 
 const days = [
   {
     id: 1,
@@ -22,6 +24,47 @@ const days = [
     spots: 0,
   },
 ];
+
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
+
+//Application component
 
 export default function Application(props) {
   const [day, setDay] = useState('Monday');
