537a25fce09d94a6cccd1e31088024d263686181
Ayisha Farishta Ashraf <Ayisha Farishta Ashraf>
2021-11-11 20:54:25 -0500

    Add stories for show and empty component

--- src/components/Appointments/index.js
+++ src/components/Appointments/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import './styles.scss'
+import Header from "./Header";
+import Empty from "./Empty";
+import Show from "./Show";
 
 export default function Appointment(props){
   return (
     <article className="appointment">
-     {props.time ? `Appointment at ${props.time}` : "No Appointments"} 
+     {props.time } 
+     {props.interview ? <Show /> : <Empty />}
     </article>
   )
 }
--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -188,3 +188,10 @@ storiesOf("InterviewerList", module)
       onSave={action("onSave")}
       onCancel={action("onCancel")}
     />))
+  .add("Appointment Empty", () => 
+    <Fragment>
+      <Appointment id={1} time="4pm"  />
+      <Appointment time="5pm" />
+    </Fragment>
+    
+    )
