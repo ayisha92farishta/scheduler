30bae5d92f94d01fbdc06f4d7df5705de0cc7dc7
Ayisha Farishta Ashraf <Ayisha Farishta Ashraf>
2021-12-06 13:52:01 -0500

    Add mock function to the appointment.test.js file

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -18,11 +18,39 @@ import Application from "components/Application";
 describe("Appointment", () => {
   /*
   A test that renders a React Component
+  In case of skipping --> xit or test.skip
 */
-  it.skip("renders without crashing", () => {
+
+  it("renders without crashing", () => {
   render(<Application />);
 });
 
 //next test
+
+})
+
+describe("Mock-function", () => {
+  it("doesn't call the function", () => {
+    const fn = jest.fn();
+    expect(fn).toHaveBeenCalledTimes(0);
+  })
+  it("calls the function", () => {
+    const fn = jest.fn();
+    fn()
+    expect(fn).toHaveBeenCalledTimes(1);
+  })
+
+  it("calls the function with specific arguments", () => {
+    const fn = jest.fn();
+    fn(10);
+    expect(fn).toHaveBeenCalledWith(10);
+   });
+
+   it("uses the mock implementation", () => {
+    const fn = jest.fn((a, b) => a*b);
+    fn(2, 2);
+    expect(fn).toHaveReturnedWith(4);
+   });
+  
 
 })
