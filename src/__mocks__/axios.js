378cdb0c346db7be2623b3bb559b652969df2106
Ayisha Farishta Ashraf <Ayisha Farishta Ashraf>
2021-12-08 14:14:40 -0500

    Add test to Application_test.js

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -53,8 +53,8 @@ const fixtures = {
   }
 };
 
-
 export default {
+  defaults: { baseURL: "4" },
   get: jest.fn(url => {
     if (url === "/api/days") {
       return Promise.resolve({
@@ -65,6 +65,7 @@ export default {
     }
 
     if (url === "/api/appointments") {
+      /* Resolve appointments data */
       return Promise.resolve({
         status: 200,
         statusText: "OK",
@@ -73,6 +74,7 @@ export default {
     }
 
     if (url === "/api/interviewers") {
+      /* Resolve interviewers data */
       return Promise.resolve({
         status: 200,
         statusText: "OK",
@@ -80,4 +82,4 @@ export default {
       });
     }
   })
-}
+};
