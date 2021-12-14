599b76a61a9fbb5aa48a5ed8932f2525a696a72c
Ayisha Farishta Ashraf <Ayisha Farishta Ashraf>
2021-12-14 13:43:35 -0500

    refactor to put two queries into a sinle query

--- cypress/integration/1-getting-started/navigation.spec.js
+++ cypress/integration/1-getting-started/navigation.spec.js
@@ -4,8 +4,13 @@ describe("Navigation", () => {
   });
   it("should navigate to Tuesday", () => {
     cy.visit("/");
-    cy.pause()
-    //navigate to "Tuesday" and clicks on it
-    cy.get("li").contains('Tuesday').click();
+    //cy.pause()
+    // //navigate to "Tuesday" and clicks on it
+    // cy.get("li").contains('Tuesday').click();
+    // //checks background color for list item
+    // cy.contains("li", "Tuesday").should("have.css", "background-color", "rgb(242, 242, 242)");
+
+    //refactor to chain the above two commands into a single line
+    cy.contains("li", "Tuesday").click().should("have.css", "background-color", "rgb(242, 242, 242)");
   })
 });
