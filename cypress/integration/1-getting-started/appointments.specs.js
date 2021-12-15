1227b33405679b068d5095f4da47416c90cc7e40
Ayisha Farishta Ashraf <Ayisha Farishta Ashraf>
2021-12-14 19:42:43 -0500

    Add new test should edit and interview

--- cypress/integration/1-getting-started/appointments.specs.js
+++ cypress/integration/1-getting-started/appointments.specs.js
@@ -1,6 +1,7 @@
 const { CYCLIC_KEY } = require("@storybook/addon-actions/dist/constants")
 
 describe("Appointments", () => {
+  //function that will run before each individual tests
   beforeEach(() => {
     cy.request("GET", "/api/debug/reset");
   
@@ -28,5 +29,24 @@ describe("Appointments", () => {
     //shows the component
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Sylvia Palmer");
+  });
+
+  it("should edit an interview", () => {
+     cy.get('[alt="Edit"]').first().click({force: true})
+    cy.pause()
+      //Find placeholder
+    cy.get("[data-testid=student-name-input]")
+    //clears input area
+    .clear()
+    //type in new name
+    .type("James Bond")
+    //selects new interviewer
+    cy.get("[alt='Tori Malcolm']").click()
+    //clicks the save button
+    cy.contains("Save").click()
+     //shows the component
+     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+     cy.contains(".appointment__card--show", "Sylvia Palmer");
+
   })
 })
